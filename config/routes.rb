8bb00cfba2997cf0c6ab66d8f9d8e2260adbcd84
Mike McGee <michael.d.mcgee@gmail.com>
2011-10-18 08:39:01 -0500

    done with the demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
-Ch2Demo::Application.routes.draw do
+  Ch2Demo::Application.routes.draw do
+  resources :microposts
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
